32f84e44b4fc03114e70617016bac3fe31d5941f
kharis12-web <brayensoulty22@gmail.com>
2021-04-25 01:23:20 +0700

    setting allow host

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aw=%fl(=n$r@bci1ct$^wxnc-w#i_lr$jx_b9%(^6&j%^5l-v1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
